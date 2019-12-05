798a819830467e0d38f81f6f412892c64e2045f3
zn <1317476403@qq.com>
2019-12-05 21:54:26 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,54 +1,9 @@
-import React,{Component} from 'react';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './demo1';
 
-
-class Clock extends Component {
-	render() {
-		let {
-			onWakeup,
-			onSleep,
-			time
-		} = this.props
-		if(time === 9) {
-			onWakeup()
-		}
-		if(time === 21) {
-			onSleep()
-		}
-
-		return (
-			<div>
-				clock
-			</div>
-		);
-	}
-}
-
-
-class Room extends React.Component{
-
-
-
-	render() {
-
-		return (
-			<div>
-				<Clock
-					time={12}
-					onWakeup={() => {
-						alert('wake up')
-					}}
-
-					onSleep={( ) => {
-						alert("sleep")
-					}}
-				/>
-				<Person/>
-				<Bed/>
-			</div>
-		);
-	}
-}
-
-
-export default App;
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
--- src/demo1.js
+++ src/demo1.js
@@ -1,76 +1,23 @@
-import React,{Component} from 'react';
-import './App.css';
+import React,{useState} from 'react';
 
-class Action extends Component{
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: ''
-		}
-	}
-
-	render() {
-		return (
-			<div>
-				<input value={this.state.value} onChange={(e) => this.setState({value: e.target.value})}/>
-				<button onClick={() => {
-					this.props.onAdd(this.state.value)
-					this.setState({
-						value: ''
-					})
-				}}>提交</button>
-			</div>
-		);
-	}
+function App(){
+    const [value,setValue] = useState('aaa')
+    return (
+        <div>
+			<InputNumber value={value} onChange={e=>{}}/>
+			<InputNumber defaultValue={value} onChange={e=>{}}/>
+        </div>
+    )
 }
-
-class List extends Component{
-	render() {
-		return (
-			<div>
-				{this.props.data.map(item => <p key={item.id}>{item.name}</p>)}
-			</div>
-		);
-	}
+function InputNumber(){
+	return (
+		<div>
+			<input onChange={
+				(e)=>{
+					this.props.onChange(e)
+				}
+			}/>
+		</div>
+	)
 }
-
-
-class App extends React.Component{
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: [
-				{name: 'a',id:0},
-				{name: 'b',id:1},
-				{name: 'c',id:2}
-			],
-			value: ''
-		}
-	}
-
-
-	render() {
-
-		return (
-			<div>
-				 <Action
-				    onAdd={(name) => {
-				    	let {data} = this.state
-					    data.push({
-						    name,
-						    id: name
-					    })
-					    this.setState({
-						    data
-					    })
-				    }}
-				 />
-
-				 <List data={this.state.data}/>
-			</div>
-		);
-	}
-}
-
-
 export default App;
