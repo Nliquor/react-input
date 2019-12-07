5ed51e7917349dc1b0d7690386e22226d9834c99
zn <1317476403@qq.com>
2019-12-07 14:15:05 +0800

    react 受控与非受控组件

--- src/demo1.js
+++ src/demo1.js
@@ -1,23 +1,37 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
+/*
+	受控 value onChange 
+	非受控 defaultValue
+*/
+const  InputNumber = (props) =>{
+	let [value,setinnerValue] = useState(
+		props.defaultValue?props.defaultValue:props.value
+	)
+	useEffect(()=>{
+		props.value && setinnerValue(props.value)
+	},[props])
+	return (
+		<div>
+			<input value={value} onChange={(e)=>{
+				if(props.defaultValue){
+					setinnerValue(e.target.value)
+				}
+				props.onChange(e)
+			}}/>
+		</div>
+	)
+
+}
 
 function App(){
     const [value,setValue] = useState('aaa')
     return (
         <div>
-			<InputNumber value={value} onChange={e=>{}}/>
-			<InputNumber defaultValue={value} onChange={e=>{}}/>
+        <InputNumber value={value} onChange={e=>{
+			// setValue(e.target.value)
+		}}/>
+        <InputNumber defaultValue={value} onChange={e=>{}}/>
         </div>
     )
-}
-function InputNumber(){
-	return (
-		<div>
-			<input onChange={
-				(e)=>{
-					this.props.onChange(e)
-				}
-			}/>
-		</div>
-	)
 }
 export default App;
